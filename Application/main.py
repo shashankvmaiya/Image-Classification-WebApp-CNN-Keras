3d0aeb85e03a5e780d398edfe9556cd7b524223c
shashankvmaiya <shashank.maiya@gmail.com>
2019-04-13 16:42:18 -0700

    web app files

--- Application/main.py
+++ Application/main.py
@@ -5,11 +5,24 @@ import keras
 from keras.preprocessing import image
 import base64
 import io
+import os
+from werkzeug.utils import secure_filename
 
 # from keras.models import load_model
 # import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
+
+def get_file_path_and_save(request):
+    # Get the file from post request
+    f = request.files['file']
+
+    # Save the file to ./uploads
+    basepath = os.path.dirname(__file__)
+    file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+    f.save(file_path)
+    return file_path
 
 
 @app.route("/")
@@ -18,20 +31,23 @@ def index():
 
 
 # @app.route('/predict', methods=['POST'])
-@app.route("/", methods=['POST'])
-def predict():
+# @app.route("/", methods=['POST'])
+@app.route('/predictResNet50', methods=['GET', 'POST'])
+def predictResNet50():
     model = keras.models.load_model("models/keras_mnist.h5")
     if request.method == 'POST':
-        data = request.get_json()['data']
-        data = base64.b64decode(data)
-        img_data = io.BytesIO(data)
-        img = image.load_img(img_data, target_size=(28, 28))
+        file_path = get_file_path_and_save(request)
+        # data = request.get_json()['data']
+        # data = base64.b64decode(data)
+        # img_data = io.BytesIO(data)
+        img = image.load_img(file_path, target_size=(28, 28))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         predictions = model.predict(x)
-        digit = np.argmax(predictions)
-    return render_template('results.html', prediction=digit)
+        digit = str(np.argmax(predictions))
+        return digit
+    # return render_template('results.html', prediction=digit)
 #     return jsonify({"prediction": data})
 
 
