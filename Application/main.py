642715cf2c2c3ece9f7d079399c93f61e90ae390
shashankvmaiya <shashank.maiya@gmail.com>
2019-04-13 14:53:24 -0700

    web app files

--- Application/main.py
+++ Application/main.py
@@ -1,42 +1,37 @@
 from flask import Flask, render_template, request, url_for, jsonify
-
+from keras.applications.inception_v3 import preprocess_input
 import numpy as np
 import keras
 from keras.preprocessing import image
-import matplotlib
+import base64
+import io
 
 # from keras.models import load_model
 # import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-def get_file_path_and_save(request):
-    # Get the file from post request
-    f = request.files['file']
-
-    # Save the file to ./uploads
-    basepath = os.path.dirname(__file__)
-    file_path = os.path.join(
-        basepath, 'uploads', secure_filename(f.filename))
-    f.save(file_path)
-    return file_path
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# @app.route("/", methods=['POST'])
-@app.route('/predict', methods=['POST'])
+
+# @app.route('/predict', methods=['POST'])
+@app.route("/", methods=['POST'])
 def predict():
-    model = keras.models.load_model("keras_mnist.h5")
+    model = keras.models.load_model("models/keras_mnist.h5")
     if request.method == 'POST':
-        comment = request.form['comment']
-        data = [comment]
-        # data = request.data['input']
-        # img = matplotlib.pyplot.plt.imread(data)
-        # result = model.predict(img)
-        # digit = np.argmax(result)
-    return render_template('results.html', prediction=data, comment=comment)
+        data = request.get_json()['data']
+        data = base64.b64decode(data)
+        img_data = io.BytesIO(data)
+        img = image.load_img(img_data, target_size=(28, 28))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        predictions = model.predict(x)
+        digit = np.argmax(predictions)
+    return render_template('results.html', prediction=digit)
 #     return jsonify({"prediction": data})
 
 
