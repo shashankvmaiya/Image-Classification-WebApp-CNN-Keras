c27c54d8f5ccc1b621130f19c4535223b33fa4a6
shashankvmaiya <shashank.maiya@gmail.com>
2019-04-13 18:56:47 -0700

    web app file modifications

--- Application/main.py
+++ Application/main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request, url_for, jsonify
 from keras.applications.inception_v3 import preprocess_input
 import numpy as np
-import keras
+from keras.models import load_model
 from keras.preprocessing import image
 import base64
 import io
 import os
+import sys
 from werkzeug.utils import secure_filename
+
 
 # from keras.models import load_model
 # import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
+print('About to load the model')
+MODEL_PATH = 'models/keras_mnist.h5'
+model = load_model(MODEL_PATH)
+print('Model loaded')
 
 
 def get_file_path_and_save(request):
@@ -22,6 +29,7 @@ def get_file_path_and_save(request):
     basepath = os.path.dirname(__file__)
     file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
     f.save(file_path)
+    print('Saved image at path: ', file_path)
     return file_path
 
 
@@ -34,18 +42,25 @@ def index():
 # @app.route("/", methods=['POST'])
 @app.route('/predictResNet50', methods=['GET', 'POST'])
 def predictResNet50():
-    model = keras.models.load_model("models/keras_mnist.h5")
+    print('----------Entered predictResNet50---------')
+    # basepath = os.path.dirname(__file__)
+    # model_path = os.path.join(basepath, 'keras_mnist.h5')
+    # print('Model Path: ', model_path)
+    # model = keras.models.load_model(model_path)
+    # print('Model loaded')
     if request.method == 'POST':
         file_path = get_file_path_and_save(request)
         # data = request.get_json()['data']
         # data = base64.b64decode(data)
         # img_data = io.BytesIO(data)
-        img = image.load_img(file_path, target_size=(28, 28))
-        x = image.img_to_array(img)
+        img = image.load_img(file_path, target_size=(28, 28), grayscale=True)
+        x = (255-image.img_to_array(img))/255.0
         x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-        predictions = model.predict(x)
+        print('Image Post-processing complete')
+        predictions = keras_mnist.predict(x)
+        print('Predictions completed, llrs = ', predictions)
         digit = str(np.argmax(predictions))
+        print('Predicted digit = ', digit)
         return digit
     # return render_template('results.html', prediction=digit)
 #     return jsonify({"prediction": data})
